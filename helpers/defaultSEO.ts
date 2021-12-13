855075217290bd099e58a14bc1eb765ce9a819bf
Joehoel <31251240+Joehoel@users.noreply.github.com>
2021-12-13 11:21:36 +0100

    Update default SEO

--- helpers/defaultSEO.ts
+++ helpers/defaultSEO.ts
@@ -1,15 +1,15 @@
 import { NextSeoProps } from "next-seo";
 
 const defaultSEO: NextSeoProps = {
-  titleTemplate: "%s | ShortURL",
-  defaultTitle: "ShortURL",
-  description: "An app made to shorten long links",
+  titleTemplate: "%s",
+  defaultTitle: "longux.xyz",
+  description: "URL Shortener app by Joël Kuijper",
   canonical: `${process.env.NEXT_PUBLIC_ORIGIN}/`,
 
   openGraph: {
     url: `${process.env.NEXT_PUBLIC_ORIGIN}/`,
-    title: "ShortURL",
-    description: "An app made to shorten long links",
+    title: "URL Shortener",
+    description: "URL Shortener app by Joël Kuijper",
     type: "website",
   },
 };
